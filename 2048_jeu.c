d3978b13512a8b7a1db02776bcf216c126b80132
IceScream-python <79906543+IceScream-python@users.noreply.github.com>
2022-10-31 00:57:03 +0100

    ajout d'un readme

--- 2048_jeu.c
+++ 2048_jeu.c
@@ -18,16 +18,30 @@ int main()
 	while (!victoire(grille))
 	{
 		char tmp[256] ;
+		printf("\nQuel coup voulez vous jouer?\nd=droite; z=haut; q=gauche; s=bas\n")
         scanf("%s", tmp) ;
         char touche = tmp[0] ;
         //mouvements : 0=droite; 1=haut; 2=gauche; 3=bas
-        if(touche == 'z') mouvement(grille, 1) ;
-        if(touche == 'q') mouvement(grille, 2) ;
-        if(touche == 's') mouvement(grille, 3) ;
-        if(touche == 'd') mouvement(grille, 0) ;
+        if(touche == 'z') 
+		{
+			mouvement(grille, 1);
+		}
+        else if(touche == 'q') 
+		{
+			mouvement(grille, 2);
+		}
+        else if(touche == 's') 
+		{
+			mouvement(grille, 3);
+		}
+        else if(touche == 'd')
+		{
+			mouvement(grille, 0);
+		} 
         rajoute_2(grille);
 		affichage(grille) ;
 	}
+	printf("Partie Terminée\n")
 	free(grille);
 	return 0 ;
 }
